5446e0ccf371161bad58e94d55395dcbbab7c5b6
xalvaine <xalvaine@gmail.com>
2024-09-10 00:58:49 +0300

    [feat] Add tablet offer

--- src/widgets/offer/ui/offer.tsx
+++ src/widgets/offer/ui/offer.tsx
@@ -1,21 +1,30 @@
 import Plate from '../assets/plate.svg'
 import Plate1 from '../assets/plate-1.svg'
 import Plate2 from '../assets/plate-2.svg'
+import PlateT from '../assets/plate-tablet.svg'
 import Separator from '../assets/separator.svg'
 import Star from '../assets/star.svg'
+import OfferList from '../assets/offer-list.svg'
+import PlateT2 from '../assets/plate-t.svg'
 import { Number } from '@/widgets/offer/ui/number'
 
 export const Offer = () => {
   return (
-    <div className='bg-gradient-to-b from-mercury-200 to-mercury-50 pb-14 rounded-b-3xl relative'>
+    <div className='bg-gradient-to-b to-mercury-200 from-mercury-50 pb-14 rounded-b-3xl relative md:h-[1458px]'>
       <div className='relative mb-12'>
-        <Plate className='text-tory-blue-950' />
-        <p className='w-96 absolute top-20 pl-4 text-3xl leading-none font-bold'>
+        <Plate className='text-tory-blue-950 md:hidden' />
+        <PlateT className='text-tory-blue-950 hidden md:block' />
+        <p
+          className={
+            'w-96 absolute top-20 pl-4 text-3xl leading-none font-bold ' +
+            'md:text-[46px] md:px-5 md:w-[680px] md:top-16'
+          }
+        >
           Индивидуально сопровождаем до&nbsp;оффера всех, кто&nbsp;набрал
           высокий балл за тест
         </p>
       </div>
-      <div className='flex flex-col items-center gap-5'>
+      <div className='flex flex-col items-center gap-5 md:hidden'>
         <div className='relative'>
           <Plate1 />
           <div className='absolute inset-0 m-auto h-fit flex flex-col items-center gap-5'>
@@ -84,6 +93,12 @@ export const Offer = () => {
           </div>
         </div>
       </div>
+      <OfferList className={'mx-auto relative z-10'} />
+      <PlateT2
+        className={
+          'absolute -bottom-[161px] mx-auto hidden w-fit md:block md:left-0 md:right-0'
+        }
+      />
     </div>
   )
 }
