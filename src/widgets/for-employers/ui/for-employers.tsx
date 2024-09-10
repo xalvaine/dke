5b009dbfe0591c7f9322e19d231a02b9aa4c622e
xalvaine <xalvaine@gmail.com>
2024-09-10 03:03:25 +0300

    [feat] Add tablet for-employers

--- src/widgets/for-employers/ui/for-employers.tsx
+++ src/widgets/for-employers/ui/for-employers.tsx
@@ -2,9 +2,11 @@ import { Button } from '@/shared/ui'
 
 export const ForEmployers = () => {
   return (
-    <div className='bg-tory-blue-950 pt-48 -mt-24 px-4 pb-10'>
-      <div className='bg-gradient-to-b from-malachite-300 to-malachite-400 rounded-3xl py-12 px-7'>
-        <h2 className='font-bold text-4xl leading-none mb-8'>Работодателям</h2>
+    <div className='bg-tory-blue-950 pt-48 -mt-24 px-4 pb-10 md:px-5'>
+      <div className='bg-gradient-to-b from-malachite-400 to-malachite-950 rounded-3xl py-12 px-7'>
+        <h2 className='font-bold text-4xl leading-none mb-8 md:text-center'>
+          Работодателям
+        </h2>
         <p className='text-center text-xl leading-6 mb-12'>
           Нужны талантливые специалисты? Оставьте запрос на кадры, мы подберём
           кандидатов точно под ваши требования.
