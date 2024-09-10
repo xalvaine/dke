2c66cfe6c003c9312dfa865fa2aab0e402100399
xalvaine <xalvaine@gmail.com>
2024-09-10 03:12:07 +0300

    [feat] Add tablet footer

--- src/widgets/footer/ui/footer.tsx
+++ src/widgets/footer/ui/footer.tsx
@@ -2,9 +2,19 @@ import DKELogo from './dke-ogo.svg'
 
 export const Footer = () => {
   return (
-    <div className='-mt-8 p-7 rounded-t-3xl relative bg-gradient-to-b from-mercury-200 to-mercury-50 flex flex-col items-center gap-7'>
+    <div
+      className={
+        '-mt-8 p-7 rounded-t-3xl relative bg-gradient-to-b from-mercury-200 to-mercury-50 flex flex-col items-center gap-7 ' +
+        'md:flex-row md:justify-between'
+      }
+    >
       <DKELogo className='w-40' />
-      <div className='text-tory-blue-900 underline flex flex-col gap-4 items-center font-light'>
+      <div
+        className={
+          'text-tory-blue-900 underline flex flex-col gap-4 items-center font-light ' +
+          'md:grid md:gap-x-[54px] md:mb-[18px] md:grid-cols-2'
+        }
+      >
         <a>Положение о проведении ДКЭ</a>
         <a>Правила участия</a>
         <a>Договор оферты</a>
