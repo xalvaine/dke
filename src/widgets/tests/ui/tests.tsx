63e8bbf5ef63b123981038c50c0c262e55467e60
xalvaine <xalvaine@gmail.com>
2024-09-12 02:43:47 +0300

    [feat] Add tests desktop

--- src/widgets/tests/ui/tests.tsx
+++ src/widgets/tests/ui/tests.tsx
@@ -1,6 +1,7 @@
 import Top from '../assets/top.svg'
 import Plate from '../assets/plate.svg'
 import Plate2 from '../assets/plate-2.svg'
+import BluePlatePC from '../assets/blue-plate-pc.svg'
 import Right from '../assets/right.svg'
 import Specialist from '../assets/specialist.png'
 import TestsStatsT from '../assets/tests-stats-t.svg'
@@ -9,10 +10,25 @@ import { Button, Slider } from '@/shared/ui'
 
 export const Tests = () => {
   return (
-    <div className='bg-gradient-to-b from-mercury-200 to-mercury-50 px-4 pb-14 rounded-b-3xl relative'>
-      <Plate2 className='absolute -bottom-9 h-9 text-mercury-50 right-14 md:w-[191px] md:h-auto md:-bottom-[56px]' />
+    <div
+      className={
+        'bg-gradient-to-b from-mercury-200 to-mercury-50 px-4 pb-14 rounded-b-3xl relative ' +
+        'lg:px-[calc((100%-904px)/2)]'
+      }
+    >
+      <Plate2
+        className={
+          'absolute -bottom-9 h-9 text-mercury-50 right-14 ' +
+          'md:w-[191px] md:h-auto md:-bottom-[56px] '
+        }
+      />
       <Top className='w-full text-tory-blue-950 -mx-4' />
-      <h2 className='mt-14 text-tory-blue-900 text-3xl leading-none font-bold mb-12'>
+      <h2
+        className={
+          'mt-14 text-tory-blue-900 text-3xl leading-none font-bold mb-12 ' +
+          'lg:text-[46px] lg:w-[780px]'
+        }
+      >
         Тестов — два вида на выбор: для студентов и специалистов
       </h2>
       <div className='flex flex-col gap-6 md:hidden'>
@@ -59,19 +75,22 @@ export const Tests = () => {
       <p
         className={
           'pr-24 text-[26px] leading-none text-black relative -mr-4 font-bold mb-8 ' +
-          'md:pr-[440px] md:text-[38px]'
+          'md:pr-[440px] md:text-[38px] ' +
+          'lg:mb-12'
         }
       >
         ДКЭ можно сдать по&nbsp;
         <span className='text-malachite-400'>70&nbsp;профессиям</span> в&nbsp;
         <span className='text-tory-blue-900'>7&nbsp;направлениях</span>
-        <Right className='absolute right-0 inset-y-0 my-auto' />
-        <BluePlateT className='absolute right-0 inset-y-0 my-auto' />
+        <Right className='absolute right-0 inset-y-0 my-auto md:hidden' />
+        <BluePlateT className='absolute right-0 inset-y-0 my-auto lg:hidden' />
+        <BluePlatePC className='absolute inset-y-0 hidden lg:block my-auto right-[calc((904px-100dvw)/2+16px)]' />
       </p>
       <ul
         className={
           'flex flex-col gap-2 items-start mb-11 ' +
-          'md:flex-row md:flex-wrap gap-x-4 gap-y-5 md:mb-[60px]'
+          'md:flex-row md:flex-wrap gap-x-4 gap-y-5 md:mb-[60px] ' +
+          'lg:w-[787px]'
         }
       >
         {[
@@ -87,7 +106,7 @@ export const Tests = () => {
             key={text}
             className={
               'px-5 py-3 bg-tory-blue-900 rounded-3xl flex items-center ' +
-              'md:h-[68px] md:rounded-[30px] md:text-[22px]'
+              'md:h-[68px] md:rounded-[30px] md:text-[22px] '
             }
           >
             {text}
@@ -105,7 +124,7 @@ export const Tests = () => {
         работодателей — о чём спрашивают в&nbsp;тесте, пригодится на&nbsp;работе
       </p>
       <Slider
-        className='-mx-4'
+        className={'-mx-4 ' + 'lg:mx-[calc((904px-100dvw)/2)]'}
         items={[Specialist.src, Specialist.src].map((_item, index) => (
           <img
             key={index}
