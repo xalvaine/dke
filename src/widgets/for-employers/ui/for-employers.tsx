e0c75ed5242cc32a35f80db16f1cc2e838b2701d
xalvaine <xalvaine@gmail.com>
2024-09-12 02:55:08 +0300

    [feat] Add for-employers desktop

--- src/widgets/for-employers/ui/for-employers.tsx
+++ src/widgets/for-employers/ui/for-employers.tsx
@@ -2,8 +2,17 @@ import { Button } from '@/shared/ui'
 
 export const ForEmployers = () => {
   return (
-    <div className='bg-tory-blue-950 pt-48 -mt-24 px-4 pb-10 md:px-5'>
-      <div className='bg-gradient-to-b from-malachite-400 to-malachite-950 rounded-3xl py-12 px-7'>
+    <div
+      className={
+        'bg-tory-blue-950 pt-48 -mt-24 px-4 pb-10 ' + 'md:px-5 ' + 'lg:px-0'
+      }
+    >
+      <div
+        className={
+          'bg-gradient-to-b from-malachite-400 to-malachite-950 rounded-3xl py-12 px-7 ' +
+          'lg:w-[904px] lg:mx-auto'
+        }
+      >
         <h2 className='font-bold text-4xl leading-none mb-8 md:text-center'>
           Работодателям
         </h2>
