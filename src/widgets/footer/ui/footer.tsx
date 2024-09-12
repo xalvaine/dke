32e4943d28278e709c1d71d9898fe9d9d84c0d4e
xalvaine <xalvaine@gmail.com>
2024-09-12 03:18:23 +0300

    [feat] Add footer desktop

--- src/widgets/footer/ui/footer.tsx
+++ src/widgets/footer/ui/footer.tsx
@@ -5,14 +5,16 @@ export const Footer = () => {
     <div
       className={
         '-mt-8 p-7 rounded-t-3xl relative bg-gradient-to-b from-mercury-200 to-mercury-50 flex flex-col items-center gap-7 ' +
-        'md:flex-row md:justify-between'
+        'md:flex-row md:justify-between ' +
+        'lg:px-[calc((100%-904px)/2)]'
       }
     >
       <DKELogo className='w-40' />
       <div
         className={
           'text-tory-blue-900 underline flex flex-col gap-4 items-center font-light ' +
-          'md:grid md:gap-x-[54px] md:mb-[18px] md:grid-cols-2'
+          'md:grid md:gap-x-[54px] md:mb-[18px] md:grid-cols-2 ' +
+          'lg:flex lg:flex-row lg:whitespace-nowrap lg:m-0'
         }
       >
         <a>Положение о проведении ДКЭ</a>
