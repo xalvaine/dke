144094ae3866b11f180de35994caeffaa474c4cf
xalvaine <xalvaine@gmail.com>
2024-09-13 11:53:24 +0300

    [fix] Fix videos

--- src/widgets/header/ui/videos.tsx
+++ src/widgets/header/ui/videos.tsx
@@ -1,18 +1,20 @@
 import { cn, Slider, useMedia } from '@/shared/ui'
-import { useRef, useState } from 'react'
 
 const Video = ({ src }: { src: string }) => {
   return (
-    <video
-      controls
+    <iframe
+      src={src}
       className='rounded-xl aspect-video object-contain bg-black w-[297px] md:w-[628px]'
-    >
-      <source src={src} type='video/mp4' />
-    </video>
+    />
   )
 }
 
-const videos = ['', '', '', '']
+const videos = [
+  'https://vk.com/video_ext.php?oid=-172706962&id=456239233&hd=2&autoplay=0',
+  'https://vk.com/video_ext.php?oid=-172706962&id=456239233&hd=2&autoplay=0',
+  'https://vk.com/video_ext.php?oid=-172706962&id=456239233&hd=2&autoplay=0',
+  'https://vk.com/video_ext.php?oid=-172706962&id=456239233&hd=2&autoplay=0',
+]
 
 export const Videos = () => {
   const { isTablet, isDesktop } = useMedia()
